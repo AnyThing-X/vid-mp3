80e1bf3e9f63129a8fa1bfa315bbebc9008e89b4
AnyThingElse <57551358+AnyThing-X@users.noreply.github.com>
2020-06-13 01:12:40 +0200

    Update video2mp3.py

--- video2mp3.py
+++ video2mp3.py
@@ -9,11 +9,16 @@ from moviepy.editor import *
 from telethon import TelegramClient, events, errors
 from telethon.tl.types import DocumentAttributeVideo
 logging.basicConfig(level=logging.WARNING)
+api_id = 17349
+api_hash = "344583e45741c457fe1862106095a5eb"
+client = TelegramClient("video2mp3", api_id, api_hash)
+client.start(bot_token='925523949:AAHv7u7vYkqlq7TACCM25S8VNUlJjobkSDQ')
 api_id = environ["api_id"]
 api_hash = environ["api_hash"]
 client = TelegramClient("video2mp3", api_id, api_hash)
 client.start(bot_token=environ["bot_token"])
 rand = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
+in_use = False
 
 
 @client.on(events.NewMessage(func=lambda e: e.is_private, pattern="/start"))
@@ -24,6 +29,11 @@ async def start_it(event):
 @client.on(events.NewMessage(func=lambda e: e.is_private and e.media))
 async def tint_it(event):
     try:
+        global in_use
+        if in_use:
+            await event.reply(f"البوت حاليا مستخدم من شخص")
+            return
+        in_use = True
         message = await event.reply("🔄 Processing : 📥 Downloading ...")
         media = await client.download_media(event.media)
         await message.edit("🔄 Processing : Converting to Mp3 file 🎙")
@@ -38,5 +48,6 @@ async def tint_it(event):
         traceback.print_exc()
         os.remove(media)
         await event.reply("🚫 Something went wrong please try again.")
-
+    finally:
+        in_use = False
 client.run_until_disconnected()
